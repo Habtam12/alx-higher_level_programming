2171e993e8c6b436c3e3b25f741c151c7a540e00
Habtam12 <habtamwube@gmail.com>
2023-03-28 09:15:45 +0000

    class file fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,1 +1,10 @@
-"Empty class that defines a square"
+#!/usr/bin/python3
+"Square module.
+This module contains a class that defines a square.
+"
+
+
+class Square():
+    "Defines a square."
+
+    pass
